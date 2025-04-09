480e9cb5a71741d4a36f1932dd0e2c417f2bcd22
Ducthang2k5 <thangbautroi1306@gmail.com>
2025-04-09 12:40:52 +0700

    update

--- app/src/main/java/com/example/duan1_nhom7/home/HomeFragment.java
+++ app/src/main/java/com/example/duan1_nhom7/home/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.duan1_nhom7.home;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -17,10 +18,12 @@ import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.widget.ViewPager2;
 
 import com.example.duan1_nhom7.Cart.CartActivity;
 import com.example.duan1_nhom7.Product.Product;
 import com.example.duan1_nhom7.Product.ProductAdapter;
+import com.example.duan1_nhom7.Product.SliderAdapter;
 import com.example.duan1_nhom7.R;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -36,7 +39,29 @@ public class HomeFragment extends Fragment {
     private List<Product> productList;
     private FirebaseFirestore db;
 
+    private ViewPager2 viewPager2;
+
     private ImageButton btnCartt;
+
+    private OnCartClickListener mListener;
+
+    public interface OnCartClickListener {
+        void onCartClicked();
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        if (context instanceof OnCartClickListener) {
+            mListener = (OnCartClickListener) context;
+        } else {
+            throw new RuntimeException(context.toString() + " must implement OnCartClickListener");
+        }
+    }
+
+
+
+    
 
 
 
@@ -46,6 +71,7 @@ public class HomeFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
         recyclerView = view.findViewById(R.id.recyclerView);
+        viewPager2 = view.findViewById(R.id.viewPager);
         recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 2));
 
         productList = new ArrayList<>();
@@ -53,8 +79,13 @@ public class HomeFragment extends Fragment {
         recyclerView.setAdapter(adapter);
 
 
+        List<String> imageUrls = new ArrayList<>();
+        imageUrls.add("https://gongcha.com.vn/wp-content/uploads/2022/06/Tra-sua-tran-chau-HK.png");
+        imageUrls.add("https://maycha.com.vn/wp-content/uploads/2023/10/tra-sua-tran-chau-hoang-kim.png");
+        imageUrls.add("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLro0x-F5aknipUJxYv9cE3eOuctbuk6-Ymw&s");
 
-
+        SliderAdapter sliderAdapter = new SliderAdapter(getContext(), imageUrls);
+        viewPager2.setAdapter(sliderAdapter);
 
 
         db = FirebaseFirestore.getInstance();
@@ -70,35 +101,30 @@ public class HomeFragment extends Fragment {
         listenForProductUpdates();
 
 
-
         btnCartt = view.findViewById(R.id.btnCart);
-
         if (btnCartt != null) {
             btnCartt.setOnClickListener(v -> {
-                // Khi nhấn nút giỏ hàng, chuyển sang CartActivity
-                Intent intent = new Intent(getActivity(), CartActivity.class);
-                startActivity(intent);
+                // Gọi phương thức từ HomeActivity để mở CartActivity
+                if (mListener != null) {
+                    mListener.onCartClicked();
+                }
             });
         } else {
-            Log.e("HomeFragment", "btnAddToCart is not found!");
+            Log.e("HomeFragment", "btnCart is not found!");
         }
-
 
 
         return view;
 
 
-
-
-
     }
 
-    private void replaceFragment(Fragment fragment) {
-        FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.fragmentContainer, fragment); // Thay thế fragment hiện tại bằng CartFragment
-        transaction.addToBackStack(null); // Nếu muốn có thể quay lại fragment trước đó
-        transaction.commit(); // Áp dụng transaction
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
     }
+
 
     private void listenForProductUpdates() {
         db.collection("products")
@@ -123,10 +149,10 @@ public class HomeFragment extends Fragment {
         FirebaseFirestore db = FirebaseFirestore.getInstance();
 
         List<Product> sampleProducts = new ArrayList<>();
-        sampleProducts.add(new Product("1", "Trà sữa hoàng kim ", "https://simexcodl.com.vn/wp-content/uploads/2024/05/tra-sua-ca-phe-2.jpg", 25000));
-        sampleProducts.add(new Product("2", "Trà sữa bạc hà ", "https://simexcodl.com.vn/wp-content/uploads/2024/05/tra-sua-ca-phe-2.jpg", 30000));
-        sampleProducts.add(new Product("3", "Trà sữa trân trâu đường đen ", "https://simexcodl.com.vn/wp-content/uploads/2024/05/tra-sua-ca-phe-2.jpg", 36000));
-        sampleProducts.add(new Product("4", "Trà sữa khoai môn ", "https://simexcodl.com.vn/wp-content/uploads/2024/05/tra-sua-ca-phe-2.jpg", 35000));
+        sampleProducts.add(new Product("1", "Trà sữa hoàng kim ", "https://gongcha.com.vn/wp-content/uploads/2022/06/Tra-sua-tran-chau-HK.png", 25000));
+        sampleProducts.add(new Product("2", "Trà sữa bạc hà ", "https://maycha.com.vn/wp-content/uploads/2023/10/tra-sua-tran-chau-hoang-kim.png", 30000));
+        sampleProducts.add(new Product("3", "Trà sữa trân trâu đường đen ", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLro0x-F5aknipUJxYv9cE3eOuctbuk6-Ymw&s", 36000));
+        sampleProducts.add(new Product("4", "Trà sữa khoai môn ", "https://gongcha.com.vn/wp-content/uploads/2018/01/Tr%C3%A0-s%E1%BB%AFa-Khoai-m%C3%B4n-2.png", 35000));
         for (Product product : sampleProducts) {
             db.collection("products").document(product.getId())
                     .set(product)
